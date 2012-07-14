68bc832162aafebae5ab9e894e649b31129650c9
PeeHaa <info@pieterhordijk.com>
2012-07-14 17:06:01 +0200

    Added Storable interface

---
+++ lib/Yadeo/Storage/Storable.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The Storable interface. All classes which are storing data somewhere should implement this interface.
+ *
+ * PHP version 5.3
+ *
+ * @category   Yadeo
+ * @package    Storage
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    0.0.1
+ */
+namespace Yadeo\Storage;
+
+/**
+ * The Storable interface. All classes which are storing data somewhere should implement this interface.
+ *
+ * @category   Yadeo
+ * @package    Storage
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Storable
+{
+    /**
+     * Saves the data in our storage medium
+     *
+     * @param array $data The data to be saved
+     *
+     * @return void
+     */
+    public function save(array $data);
+}
