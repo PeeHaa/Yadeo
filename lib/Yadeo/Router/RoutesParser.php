9f55b2b093919ec31b42bfb8af77adc4a2a2f179
PeeHaa <info@pieterhordijk.com>
2012-07-15 15:05:49 +0200

    Fixed namespace

--- lib/Yadeo/Router/RoutesParser.php
+++ lib/Yadeo/Router/RoutesParser.php
@@ -11,7 +11,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    0.0.1
  */
-namespace Yadeo\Router\Route;
+namespace Yadeo\Router;
 
 /**
  * The Parser interface. All classes which can parse routes should implement this interface.
