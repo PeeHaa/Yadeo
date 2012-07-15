06615f8e18ab9444f586176c34ac7d1444dc99c0
PeeHaa <info@pieterhordijk.com>
2012-07-15 15:58:38 +0200

    Fixed typo in namespace

--- lib/Yadeo/Router/Route.php
+++ lib/Yadeo/Router/Route.php
@@ -11,7 +11,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    0.0.1
  */
-namespace Yadoe\Router;
+namespace Yadeo\Router;
 
 /**
  * Route interface. All routes should implement this interface.
